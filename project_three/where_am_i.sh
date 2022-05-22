ad09e52d104a11baf3e1fbf85d94547d9700bb8f
blueshrapnel <karen.archer@gmail.com>
2022-05-22 09:05:14 +0100

    added files for project_three

---
+++ project_three/where_am_i.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# source='/home/karen/workspace/udacity-robotics' && target='/home/ros/'
+
+# docker build -t noetic-mcl
+source='/home/karen/workspace/udacity-robotics' && target='/home/ros'
+
+docker run -it \
+    --gpus all \
+    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --mount type=bind,source=$source,target=$target \
+    --workdir="/home/ros/project_three" \
+    --privileged \
+    --net=host \
+    noetic-mcl \
+    bash
+ 
